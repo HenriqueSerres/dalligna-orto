262e86c70e8cf1572b73e54bbf7f9ee8cf3751eb
Henrique Serres <henriqueserres@gmail.com>
2023-10-16 18:24:03 -0300

    change layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,5 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import './globals.css'
 
 export const metadata: Metadata = {
   title: "Dall'Igna Ortodontia",
@@ -15,7 +13,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className='bg-blue-200'>{children}</body>
     </html>
   )
 }
